b5cfc6261ba6d002a80f4531a1ae75c49da7281e
adarsh58 <adarsh.sharma58@yahoo.com>
2024-09-02 22:33:22 +0530

    refractor

--- src/component/Cart/ProductItem.jsx
+++ src/component/Cart/ProductItem.jsx
@@ -1,13 +1,12 @@
-import React, { useState,useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./ProductItemCart.css";
 
 const ProductItem = (props) => {
-  const item= props.product
- 
+  const item = props.product;
 
   return (
     <>
-      <div className="cartitem" >
+      <div className="cartitem">
         <div className="cartimage">
           <img src={item.thumbnail}></img>
         </div>
@@ -15,32 +14,15 @@ const ProductItem = (props) => {
           <div className="carttitle">
             <span>{item.title}</span>
           </div>
-          <div className="cartrating">
-            <span>
-              <b>Rating:</b>
-              <i>
-                {item.rating}&nbsp;<i className="bi bi-star-fill"></i>
-              </i>
-            </span>
-          </div>
           <div className="cartpricetag">
             <span className="cartpriceitem">
-            <i className="bi bi-tags-fill"></i> ${item.price}&nbsp;
+              <i className="bi bi-tags-fill"></i> ${item.price}&nbsp;
             </span>
-            <span className="cartpriceinfo">
-             Discount percentage : ${item.discountPercentage}
-            </span>
-          </div>
-          <div className="cartother">
-            <button type="button" className="btn btn-sm btn-warning">
-              In Stock <span className="cartbadge">{item.stock}</span>
-            </button>
-            
-            <div><i className="bi bi-arrow-return-left"></i>Return Policy : {item.returnPolicy}</div>
-            
           </div>
           <div className="cartcheckout">
-          <button  type="button" className="btn btn-sm  btn-dark ">Check out</button>
+            <button type="button" className="btn btn-sm  btn-dark ">
+              Check out
+            </button>
           </div>
         </div>
       </div>
