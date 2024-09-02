bf6c518c05f81d68a0c5ee9d294902cbaa46dfeb
adarsh58 <adarsh.sharma58@yahoo.com>
2024-09-02 16:47:50 +0530

    Added IndexDb delete

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
           <Link className="nav-link" aria-current="page" to="/home">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/cart">Cart</Link>
+          <Link className="nav-link" to="/cart">IndexDB</Link>
         </li>
 
       </ul>
